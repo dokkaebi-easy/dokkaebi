7ce1985e0e80b5768367ea06389af62e09eaa75e
ddddd <rlarudehd32@naver.com>
2022-04-12 22:00:41 +0900

    setting: first set

---
+++ BE/core/src/test/java/com/dockerby/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerby.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
