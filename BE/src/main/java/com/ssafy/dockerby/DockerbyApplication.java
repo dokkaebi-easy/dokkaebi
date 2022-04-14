df609529f62d209a637001f54381a341ceb41328
ddddd <rlarudehd32@naver.com>
2022-04-15 01:20:40 +0900

    setting : folder set

--- BE/src/main/java/com/ssafy/dockerby/DockerbyApplication.java
+++ BE/src/main/java/com/ssafy/dockerby/DockerbyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerbyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DockerbyApplication.class, args);
 	}
-
 }
