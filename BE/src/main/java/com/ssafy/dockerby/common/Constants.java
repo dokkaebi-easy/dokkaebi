191c1b6f1d8cc308cf5f5e5c98e539755b42b5d5
ethankim93 <wsmh.kim12@gmail.com>
2022-04-20 16:07:39 +0900

    fix : 파일 기능 브런치

---
+++ BE/src/main/java/com/ssafy/dockerby/common/Constants.java
@@ -0,0 +1,26 @@
+package com.ssafy.dockerby.common;
+
+public class Constants {
+
+    public enum ExceptionClass {
+        //ExceptionClass 설정
+        USER("User"), PROJECT("Project"),FILE("File");
+
+        private String exceptionClass;
+
+        ExceptionClass(String exceptionClass) {
+            this.exceptionClass = exceptionClass;
+        }
+
+        public String getExceptionClass() {
+            return exceptionClass;
+        }
+
+        @Override
+        public String toString() {
+            return getExceptionClass() + " Exception. ";
+        }
+
+    }
+
+}
---
+++ BE/src/main/java/com/ssafy/dockerby/common/exception/UserDefindedException.java
@@ -0,0 +1,36 @@
+package com.ssafy.dockerby.common.exception;
+
+import com.ssafy.dockerby.common.Constants;
+import org.springframework.http.HttpStatus;
+
+public class UserDefindedException extends Exception {
+
+    private static final long serialVersionUID = 4663380430591151694L;
+
+    private Constants.ExceptionClass exceptionClass;
+    private HttpStatus httpStatus;
+
+    public UserDefindedException(Constants.ExceptionClass exceptionClass, HttpStatus httpStatus,
+        String message) {
+        super(exceptionClass.toString() + message);
+        this.exceptionClass = exceptionClass;
+        this.httpStatus = httpStatus;
+    }
+
+    public Constants.ExceptionClass getExceptionClass() {
+        return exceptionClass;
+    }
+
+    public int getHttpStatusCode() {
+        return httpStatus.value();
+    }
+
+    public String getHttpStatusType() {
+        return httpStatus.getReasonPhrase();
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+}
