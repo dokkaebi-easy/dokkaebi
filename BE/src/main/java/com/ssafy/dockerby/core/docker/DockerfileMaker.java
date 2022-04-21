beba34da12e2a020a1170adb1cea27d029a28766
이동호 <edh1021@gmail.com>
2022-04-21 04:24:45 +0000

    refactor: exception 처리는 함수 호출자에게 위임

---
+++ BE/src/main/java/com/ssafy/dockerby/core/docker/DockerfileMaker.java
@@ -0,0 +1,70 @@
+package com.ssafy.dockerby.core.docker;
+
+import com.ssafy.dockerby.core.docker.dto.ContainerConfig;
+import com.ssafy.dockerby.util.FileManager;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DockerfileMaker {
+
+  private final String rootDir;
+
+  public DockerfileMaker(String rootDir) {
+    this.rootDir = rootDir;
+  }
+
+  /**
+   * 환경설정 값을 이용해 Dockerfile을 생성해준다.
+   * MySQL은 Dockerfile을 생성하지 않아 바로 break 된다.
+   * @param config : Framework/Library 환경설정 값
+   */
+  public void make(ContainerConfig config) throws IOException {
+    switch (config.getFramework()) {
+      case Vue:
+        break;
+      case SpringBoot:
+        makeSpringBootDockerfile(config);
+        break;
+      case Django:
+        break;
+      case React:
+        break;
+      case MySQL:
+        // MySQL은 dockerfile 없이 docker run으로 바로 진행
+        break;
+      default:
+        throw new IllegalArgumentException("makeDockerFile : " + config.getFramework() + "Error");
+    }
+  }
+
+  private void makeSpringBootDockerfile(ContainerConfig config) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    sb.append("FROM ").append(config.getVersion()).append(' ').append("as builder").append('\n');
+    sb.append("COPY ").append(rootDir).append(config.getProjectDirectory()).append(" .").append('\n');
+    if (config.getType() == "Gradle") {
+      sb.append("RUN ").append("chmod +x ./gradlew").append('\n');
+      sb.append("RUN ").append("./gradlew clean build").append('\n');
+    } else if (config.getType() == "Maven") {
+      sb.append("RUN ").append("chmod +x ./mvnw").append('\n');
+      sb.append("RUN ").append("./mvnw clean package").append('\n');
+    }
+    sb.append("FROM ").append(config.getVersion()).append('\n');
+    sb.append("COPY --from=builder ");
+    if (config.getType() == "Gradle") {
+      sb.append(((config.getBuildPath() == null) ? "/build/libs" : config.getBuildPath()) + "/*.jar");
+    } else if (config.getType() == "Maven") {
+      sb.append(((config.getBuildPath() == null) ? "/target" : config.getBuildPath()) + "/*.jar");
+    }
+    sb.append(" app.jar").append('\n');
+    sb.append("ENTRYPOINT [\"java\", \"-jar\", \"./app.jar\"]");
+
+    StringBuilder path = new StringBuilder();
+    sb.append(rootDir).append(config.getProjectDirectory()).append('/');
+    saveDockerFile(path.toString(),sb.toString());
+  }
+
+  private void saveDockerFile(String pjtDir, String sb) throws IOException {
+      FileManager.saveFile(pjtDir, "dockerfile", sb);
+  }
+}
