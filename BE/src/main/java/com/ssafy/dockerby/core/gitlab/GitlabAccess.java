eb3dd23126fbb37707f5f3de8b672a53bef35637
soralee <chalrysr@naver.com>
2022-04-23 01:57:39 +0900

    refactor : 코드리뷰를 통해 불필요한 코드 삭제 및 주석이 필요한 부분 추가

--- BE/src/main/java/com/ssafy/dockerby/core/gitlab/GitlabAccess.java
+++ BE/src/main/java/com/ssafy/dockerby/core/gitlab/GitlabAccess.java
@@ -12,6 +12,11 @@ public class GitlabAccess {
 
     // git repository url 검증
     public static Boolean isGitlabRepositoryUrl(Object projectIdOrPath, String repositoryUrl) {
+        // 파라메터를 Object 타입으로 받아온 이유는 gitlab lib로 요청하는 함수 gitLabApi.getProjectApi().getProject(projectIdOrPath) 에서
+        // projectIdOrPath 값이 Long or String 값으로 받아서 사용하고 있기 때문에 그대로 차용했습니다.
+        // 아무래도 gitLab lib에 요청해서 사용하다보니 대체로 해당 함수의 규칙을 따라가려고 그대로 사용하였습니다.
+        // 현재 우리 코드에서는 projectIdOrPath : projectId 로 Long 타입만 사용하고 있습니다.
+
         try {
             // Get a specific project, which is owned by the authentication user.
             String gitlabHttpUrl = gitLabApi.getProjectApi().getProject(projectIdOrPath).getHttpUrlToRepo();
@@ -20,7 +25,7 @@ public class GitlabAccess {
             }
             return false;
         }catch (GitLabApiException e) {
-            log.error(e.getMessage());
+            log.error(e.getMessage(), e);
             return true;
         }
     }
@@ -33,7 +38,7 @@ public class GitlabAccess {
             gitLabApi.getRepositoryApi().getBranch(projectIdOrPath, branchName);
             return true;
         } catch (GitLabApiException e) {
-            log.error(e.getMessage());
+            log.error(e.getMessage(), e);
             return false;
         }
     }
