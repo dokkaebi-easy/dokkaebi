59a90738ad00a4da51071ebd677568386c8ebf04
Ymaru02 <dyun022@gmail.com>
2022-04-28 20:13:05 +0900

    api

--- FE/src/SetupProxy.js
+++ FE/src/SetupProxy.js
@@ -4,6 +4,9 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
+      // 로컬 확인용
+      // target: 'http://localhost:8080',
+      // 서버 확인용
       target: 'http://k6s205.p.ssafy.io:8482',
       changeOrigin: true,
     }),
